ffdee3d0b9aed6a743d5bdc4f06d0d72036b0dc2
Logan Wohlers <lawohlers@gmail.com>
2019-02-05 03:38:32 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,10 +12,17 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
+  
+  contacts.each { |k,v|
+    v.each { |k2, v2|
+      if(k2==:favorite_icecream_flavors)
+        v2.delete_if{|x| x=="strawberry"
+        }
+      end   
+        
+    }
+  }
+    
   #remember to return your newly altered contacts hash!
   contacts
 end
